8361af0848e6fb6d23253f4fdc0cac80ff00d581
Sofiia Kirichok <megasofiyka9@gmail.com>
2024-10-19 16:20:50 +0300

    Додала динамічне оновлення графіку ІМТ

--- Database/DatabaseHandler.cs
+++ Database/DatabaseHandler.cs
@@ -1,11 +1,7 @@
 ﻿using HealthApp.Database.Tables;
+using Microcharts;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+using SkiaSharp;
 
 namespace HealthApp.Database
 {
@@ -131,6 +127,89 @@ namespace HealthApp.Database
         public double CalculateBMI(double weight, double height)
         {
             return Math.Round(weight /Math.Pow(height/100, 2), 1);
+        }
+
+        public List<ChartEntry> CalculateChart()
+        {
+            var LastMetricsList = new List<double>();
+            var DateMetricsList = new List<string>();
+
+            for (int i = 1; i <= 7; i++)
+            {
+                var metrics = db.metrics.Find(i);
+                double metric;
+                string date;
+
+                if (metrics == null)
+                {
+                    metric = 0;
+                    date = "N/A";
+                }
+                else
+                {
+                    metric = metrics.bmi;
+                    date = metrics.date;
+                }
+
+                LastMetricsList.Add(metric);
+                DateMetricsList.Add(date);
+            }
+
+            var entries = new List<ChartEntry>
+    {
+        new ChartEntry((float)LastMetricsList[0]) // Індекси починаються з 0
+        {
+            Label = DateMetricsList[0],
+            ValueLabel = LastMetricsList[0].ToString(),
+            Color = SKColor.Parse("#624E88")
+        },
+        new ChartEntry((float)LastMetricsList[1])
+        {
+            Label = DateMetricsList[1],
+            ValueLabel = LastMetricsList[1].ToString(),
+            Color = SKColor.Parse("#745B9A")
+        },
+        new ChartEntry((float)LastMetricsList[2])
+        {
+            Label = DateMetricsList[2],
+            ValueLabel = LastMetricsList[2].ToString(),
+            Color = SKColor.Parse("#8967B3")
+        },
+        new ChartEntry((float)LastMetricsList[3])
+        {
+            Label = DateMetricsList[3],
+            ValueLabel = LastMetricsList[3].ToString(),
+            Color = SKColor.Parse("#A270A0")
+        },
+        new ChartEntry((float)LastMetricsList[4])
+        {
+            Label = DateMetricsList[4],
+            ValueLabel = LastMetricsList[4].ToString(),
+            Color = SKColor.Parse("#CB80AB")
+        },
+        new ChartEntry((float)LastMetricsList[5])
+        {
+            Label = DateMetricsList[5],
+            ValueLabel = LastMetricsList[5].ToString(),
+            Color = SKColor.Parse("#D39A96")
+        },
+        new ChartEntry((float)LastMetricsList[6])
+        {
+            Label = DateMetricsList[6],
+            ValueLabel = LastMetricsList[6].ToString(),
+            Color = SKColor.Parse("#E6D9A2")
+        }
+    };
+
+            return entries;
+        }
+
+
+        private float NormalizeValue(float value)
+        {
+            float min = 10;
+            float max = 50;
+            return (value - min) / (max - min) * 100;
         }
     }
 }
--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using HealthApp.BindingHelpers;
 using HealthApp.Database;
 using Microcharts;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using SkiaSharp;
 
 namespace HealthApp
 {
@@ -24,6 +21,8 @@ namespace HealthApp
             base.OnAppearing();
 
             viewModel.UpdateData();
+
+            CreateBarChart();
         }
 
         public async void ChangeParametersButtonClicked(object sender, EventArgs e)
@@ -74,78 +73,32 @@ namespace HealthApp
 
             if (int.TryParse(input, out int amount))
             {
-                    using (var db = new DatabaseSource())
-                    {
-                        var dbHandler = new DatabaseHandler();
+                using (var db = new DatabaseSource())
+                {
+                    var dbHandler = new DatabaseHandler();
 
-                        await dbHandler.AddWater(amount);
+                    await dbHandler.AddWater(amount);
 
-                        await db.SaveChangesAsync();
-                    }
-                
+                    await db.SaveChangesAsync();
+                }
+
                 OnAppearing();
             }
-                
+
         }
 
         private void CreateBarChart()
         {
-            var entries = new List<ChartEntry>
+            using (var db = new DatabaseSource())
             {
-                new ChartEntry(NormalizeValue((float)24.0))
-                {
-                    Label = "ПН",
-                    ValueLabel = "25.0",
-                    Color = SKColor.Parse("#624E88")
-                },
-                new ChartEntry(NormalizeValue((float)24.7))
-                {
-                    Label = "ВТ",
-                    ValueLabel = "25.1",
-                    Color = SKColor.Parse("#745B9A")
-                },
-                new ChartEntry(NormalizeValue((float) 25.8))
-                {
-                    Label = "СР",
-                    ValueLabel = "24.8",
-                    Color = SKColor.Parse("#8967B3")
-                },
-                new ChartEntry(NormalizeValue((float) 26.1))
-                {
-                    Label = "ЧТ",
-                    ValueLabel = "24.5",
-                    Color = SKColor.Parse("#A270A0")
-                },
-                new ChartEntry(NormalizeValue((float) 25.3))
-                {
-                    Label = "ПТ",
-                    ValueLabel = "24.3",
-                    Color = SKColor.Parse("#CB80AB")
-                },
-                new ChartEntry(NormalizeValue((float) 24.0))
-                {
-                    Label = "СБ",
-                    ValueLabel = "23.0",
-                    Color = SKColor.Parse("#D39A96")
-                },
-                new ChartEntry(NormalizeValue((float) 23.4))
-                {
-                    Label = "НД",
-                    ValueLabel = "23.4",
-                    Color = SKColor.Parse("#E6D9A2")
-                }
-            };
+                var dbHandler = new DatabaseHandler();
+                var entries = dbHandler.CalculateChart();
 
-            ChartWeek.Chart = new BarChart {
-                Entries = entries
-            }; 
-
-        }
-        public float NormalizeValue(float value)
-        {
-            float min = 10;
-            float max = 50;
-            return (value - min) / (max - min) * 100;
+                LastDaysChart.Chart = new BarChart
+                {
+                    Entries = entries
+                };
+            }
         }
     }
 }
