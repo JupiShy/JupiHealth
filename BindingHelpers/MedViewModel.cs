cb793009283e6e7b0da000edc06d60f4493eeb34
Sofiia Kirichok <megasofiyka9@gmail.com>
2024-11-03 18:25:43 +0200

    Фікс мінорних багів

--- BindingHelpers/MedViewModel.cs
+++ BindingHelpers/MedViewModel.cs
@@ -8,6 +8,7 @@ using System.Windows.Input;
 using Microsoft.EntityFrameworkCore;
 using HealthApp.Database;
 using HealthApp.Database.Tables;
+using HealthApp.CoreLogic;
 
 namespace HealthApp.BindingHelpers
 {
@@ -37,9 +38,28 @@ namespace HealthApp.BindingHelpers
 
         public async Task EditMed(Medicines medicine, string newName, int newDays, string newHours)
         {
+            var old_days_value = medicine.days_to_take;
+            var old_hours_value = medicine.reception_hours;
+            
             medicine.drug_name = newName;
             medicine.days_to_take = newDays;
             medicine.reception_hours = newHours;
+
+            var medProgress = await _db.medicines_progress.FirstOrDefaultAsync(mp => mp.med_id == medicine.id);
+
+            if (medProgress != null)
+            {
+                if(newDays != old_days_value)
+                {
+                   medProgress.day_num = newDays;
+                }
+
+                if(newHours != old_hours_value)
+                {
+                    var medHandler = new MedicinesProgressHandler();
+                    medProgress.times = medHandler.ConvertTimeString(medicine.reception_hours);
+                }
+            }
 
             _db.medicines.Update(medicine);
             await _db.SaveChangesAsync();
